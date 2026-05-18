87c699895f79ab99afbd0c05f4e8e6c4e86875c6
SkyNicky <nycholasffrocha@gmail.com>
2026-05-18 19:40:54 -0300

    configurações iniciais

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+    define('BASE_URL', 'http://localhost/API-REST-PRODUTOS-PHP');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Simple Framework</title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css"/>
+  </head>
+  <body>
+
+  <?php
+    require 'api/autoload.php';
+    
+    use api\core\App;
+    use api\core\Controller;
+
+    $app = new App();
+
+  ?>
+  <script src="/assets/js/jquery.slim.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
